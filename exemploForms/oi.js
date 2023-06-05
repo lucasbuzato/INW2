71ee91bc2d3c3515a210b34cdd98756f68c42250
Logon Aluno <logonrmlocal@fiap.com.br>
2023-06-05 12:24:57 -0300

    Exemplo Forms

---
+++ exemploForms/oi.js
@@ -0,0 +1,21 @@
+console.log("Turma\n\n\n 2MIB")
+console.log("Nome\tNOTA\tTURMA")
+console.log("Pedro\t10\t2MIB")
+
+let nom ="Pedro"
+let valor1 = 10
+let valor2 = 20
+let valor3 = valor1 + valor2
+
+nome = "Marcos"
+
+console.log("Oi mues amigo(a)" +nome.toLowerCase())
+console.log("A soma dos dois valores é: ",(valor1+valor2))
+
+const leia = require("prompt-sync")()
+let numero1,numero2 = 0
+
+numero1 = parseInt(leia ("Digite o Primeiro Numero: "))
+numero2 = parseInt(leia ("Digite o Segundo Numero: "))
+
+console.log("A soma dos Números é :"+(numero1+numero2))
