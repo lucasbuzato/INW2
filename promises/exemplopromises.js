c41fc63c02fc68d135be06c4295e1b5ee52a5819
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-12 11:31:32 -0300

    promises

---
+++ promises/exemplopromises.js
@@ -0,0 +1,11 @@
+function falarDepoisDE(segundos,frase){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            resolve(frase)
+        }, segundos * 1000)
+    })
+}
+
+falarDepoisDE(3,"Oi turma 2MIB")
+.then(frase =>frase.concat(' ?!?'))
+.then(outraFrase => console.log(outraFrase))
